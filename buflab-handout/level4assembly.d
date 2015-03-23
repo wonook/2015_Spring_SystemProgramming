dfd98f34303feba655ee89d68f72537fa7c6ea2d
Won Wook SONG <wsong0512@gmail.com>
2015-03-24 01:06:50 +0900

    assemble

--- buflab-handout/level4assembly.d
+++ buflab-handout/level4assembly.d
@@ -6,7 +6,6 @@ Disassembly of section .text:
 
 00000000 <.text>:
    0:	b8 69 46 8d 23       	mov    $0x238d4669,%eax
-   5:	8d ac 24 e8 30 00 00 	lea    0x30e8(%esp),%ebp
-   c:	83 ec 04             	sub    $0x4,%esp
-   f:	c7 04 24 6e 8e 04 08 	movl   $0x8048e6e,(%esp)
-  16:	c3                   	ret    
+   5:	83 ec 04             	sub    $0x4,%esp
+   8:	c7 04 24 6e 8e 04 08 	movl   $0x8048e6e,(%esp)
+   f:	c3                   	ret    
