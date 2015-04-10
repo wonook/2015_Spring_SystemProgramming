1faeb113287b154115cde37d9a73dd9f91069486
Won Wook SONG <wsong0512@gmail.com>
2015-04-11 01:19:19 +0900

    finish up work

--- klab/assgnmt2/msrtest.c
+++ klab/assgnmt2/msrtest.c
@@ -8,10 +8,14 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-void reset_counter (int file_desc) {
+void reset_counter (int fd);
+int select_event ();
+void start_counter (int fd);
+void stop_counter(int fd);
+int read_counter (int fd, int event);
+
+void reset_counter (int fd) {
     struct MsrInOut msr_reset[] = {
-        { MSR_WRITE, 0x38f, 0x00, 0x00 },	// ia32_perf_global_ctrl: disable 4 PMCs & 3 FFCs
-        { MSR_WRITE, 0x38d, 0x00, 0x00 },	// ia32_perf_fixed_ctrl: clean up FFC ctrls
         { MSR_WRITE, 0xc1, 0x00, 0x00 },        // ia32_pmc0: zero value (35-5)
         { MSR_WRITE, 0xc2, 0x00, 0x00 },        // ia32_pmc1: zero value (35-5)
         { MSR_WRITE, 0xc3, 0x00, 0x00 },        // ia32_pmc2: zero value (35-5)
@@ -19,40 +23,37 @@ void reset_counter (int file_desc) {
         { MSR_WRITE, 0x309, 0x00, 0x00 },       // ia32_fixed_ctr0: zero value (35-17)
         { MSR_WRITE, 0x30a, 0x00, 0x00 },       // ia32_fixed_ctr1: zero value (35-17)
         { MSR_WRITE, 0x30b, 0x00, 0x00 },       // ia32_fixed_ctr2: zero value (35-17)
+        { MSR_STOP, 0x00, 0x00 }
     };
 
-    printf ("Reseting & stopping PMU counter...\n");
-
-    ioctl (file_desc, IOCTL_MSR_CMDS, (long long)msr_reset);
-
-    printf ("Done!\n");
-
+    stop_counter(fd);
+    ioctl (fd, IOCTL_MSR_CMDS, (long long)msr_reset);
+    printf ("Reset the PMU counter.\n");
     return ;
 }
 
-int select_event (int file_desc) {
+int select_event () {
     int event;
 
-    printf ("Please select the PMU event to monitor!\n");
-    printf ("1: uops retired.\n");
-    printf ("2: uops issued.\n");
-    printf ("3. stalled cycles.\n");
-    printf ("4. resource stalls.\n");
-    printf ("5. instr retired.\n");
-    printf ("6. core cycles.\n");
-    printf ("7. ref cycles.\n");
+    printf ("Please select a PMU event to monitor");
+    printf ("(0.exit/1.uops retired/");
+    printf ("2.uops issued/");
+    printf ("3.stalled cycles/");
+    printf ("4.resource stalls/");
+    printf ("5.instr retired/");
+    printf ("6.core cycles/");
+    printf ("7.ref cycles):");
 
     while (1){
-        printf ("Type your event: ");
         scanf ("%d", &event);
-        if ((0<event)&&(event<8)) break;
-        else printf ("Incorrect input!\n");
+        if ((0<=event)&&(event<8)) break;
+        else printf ("Incorrect input!\nTry again:");
     }
 
     return event;
 }
 
-void start_counter (int file_desc) {
+void start_counter (int fd) {
     struct MsrInOut msr_start[] = {
         { MSR_WRITE, 0x186, 0x004101c2, 0x00 }, // ia32_perfevtsel1, UOPS_RETIRED.ALL (19-28)
         { MSR_WRITE, 0x187, 0x0041010e, 0x00 }, // ia32_perfevtsel0, UOPS_ISSUED.ANY (19.22)
@@ -63,16 +64,24 @@ void start_counter (int file_desc) {
         { MSR_STOP, 0x00, 0x00 }
     };
 
-    printf ("Starting PMU counter...\n");
-
-    ioctl (file_desc, IOCTL_MSR_CMDS, (long long)msr_start);
-
-    printf ("Done!\n");
-
+    ioctl (fd, IOCTL_MSR_CMDS, (long long)msr_start);
+    printf ("Started the PMU counter.\n");
     return ;
 }
 
-void read_counter (int file_desc, int event) {
+void stop_counter(int fd) {
+    struct MsrInOut msr_stop[] = {
+        { MSR_WRITE, 0x38f, 0x00, 0x00 },       // ia32_perf_global_ctrl: disable 4 PMCs & 3 FFCs
+        { MSR_WRITE, 0x38d, 0x00, 0x00 },       // ia32_perf_fixed_ctr_ctrl: clean up FFC ctrls
+        { MSR_STOP, 0x00, 0x00 }
+    };
+
+    ioctl(fd, IOCTL_MSR_CMDS, (long long)msr_stop);
+    printf("Stopped the PMU counter.\n");
+    return;
+}
+
+int read_counter (int fd, int event) {
     struct MsrInOut msr_read[] = {
         { MSR_READ, 0xc1, 0x00 },               // ia32_pmc0: read value (35-5)
         { MSR_READ, 0xc2, 0x00 },               // ia32_pmc1: read value (35-5)
@@ -84,11 +93,10 @@ void read_counter (int file_desc, int event) {
         { MSR_STOP, 0x00, 0x00 }
     };
 
-    printf ("Reading PMU counter...\n");
+    ioctl (fd, IOCTL_MSR_CMDS, (long long)msr_read);
+    printf ("Read PMU counter.\n");
 
-    ioctl (file_desc, IOCTL_MSR_CMDS, (long long)msr_read);
-
-    printf ("Done!\n");
+    if(event<0) event = select_event(fd);
 
     switch (event) {
         case 1: // uops retired
@@ -112,10 +120,12 @@ void read_counter (int file_desc, int event) {
         case 7: // ref cycles
             printf("ref cycles: ");
             break;
+        default:
+            return -1;
     }
 
     printf ("%7lld\n", msr_read[event-1].value);
-
+    return 0;
 }
 
 
@@ -151,7 +161,7 @@ static void closeDriver(int fd)
  */
 int main(void)
 {
-    int fd;
+    int fd, a;
     struct MsrInOut msr_start[] = {
         { MSR_WRITE, 0x38f, 0x00, 0x00 },       // ia32_perf_global_ctrl: disable 4 PMCs & 3 FFCs
         { MSR_WRITE, 0xc1, 0x00, 0x00 },        // ia32_pmc0: zero value (35-5)
@@ -185,16 +195,24 @@ int main(void)
 
 
     fd = loadDriver();
-    ioctl(fd, IOCTL_MSR_CMDS, (long long)msr_start);
-    printf("This is a hex number 0x%x\n", -1);
-    ioctl(fd, IOCTL_MSR_CMDS, (long long)msr_stop);
-    printf("uops retired:    %7lld\n", msr_stop[2].value);
-    printf("uops issued:     %7lld\n", msr_stop[3].value);
-    printf("stalled cycles:  %7lld\n", msr_stop[4].value);
-    printf("resource stalls: %7lld\n", msr_stop[5].value);
-    printf("instr retired:   %7lld\n", msr_stop[6].value);
-    printf("core cycles:     %7lld\n", msr_stop[7].value);
-    printf("ref cycles:      %7lld\n", msr_stop[8].value);
+/*    ioctl(fd, IOCTL_MSR_CMDS, (long long)msr_start);*/
+    /*printf("This is a hex number 0x%x\n", -1);*/
+    /*ioctl(fd, IOCTL_MSR_CMDS, (long long)msr_stop);*/
+    /*printf("uops retired:    %7lld\n", msr_stop[2].value);*/
+    /*printf("uops issued:     %7lld\n", msr_stop[3].value);*/
+    /*printf("stalled cycles:  %7lld\n", msr_stop[4].value);*/
+    /*printf("resource stalls: %7lld\n\n", msr_stop[5].value);*/
+    /*printf("instr retired:   %7lld\n", msr_stop[6].value);*/
+    /*printf("core cycles:     %7lld\n", msr_stop[7].value);*/
+    /*printf("ref cycles:      %7lld\n\n", msr_stop[8].value);*/
+    reset_counter(fd);
+    start_counter(fd);
+    printf("printing a hex number for testing:0x%x\n", -1);
+    stop_counter(fd);
+    while(1) {
+        a = read_counter(fd, select_event());
+        if(a<0) break;
+    }
     closeDriver(fd);
     return 0;
 }
