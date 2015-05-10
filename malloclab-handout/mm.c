2c085eb721e51c99b42c8e1ce777abf3a7c57546
Won Wook SONG <wsong0512@gmail.com>
2015-05-10 15:37:57 +0900

    fix mistakes and add debugging tools

--- malloclab-handout/mm.c
+++ malloclab-handout/mm.c
@@ -62,8 +62,9 @@ static range_t **gl_ranges;
 #define PUT(p, val) (*(unsigned int *)(p) = (val))
 
 /* Read the size and the allocated bit from address p (p must be header/footer) */
-#define GET_SIZE(p) (GET(p) & ~0x7)
-#define GET_ALLOC(p) (GET(p) & 0x1)
+#define GET_SIZE(p)   (GET(p) & ~0x7)
+#define GET_ALLOC(p)  (GET(p) & 0x1)
+#define GET_ALLOC_CHAR(p) ((GET_ALLOC(p)) ? 'A' : 'F' )
 
 /* Given block ptr bp, compute address of its header and footer */
 #define HDRP(bp)    ((char *)(bp) - WSIZE)
@@ -78,8 +79,11 @@ static char *heap_listp = 0;
 
 /* OWN FUNCTIONS */
 #define DEBUG //for debugging
+#define DEBUG_STATUS
 
 #define BLK_SIZE(bp) (GET_SIZE(HDRP(bp)))
+#define BLK_ALLOC(bp) (GET_ALLOC(HDRP(bp)))
+#define BLK_ALLOC_CHAR(bp) ((BLK_ALLOC(bp)) ? 'A' : 'F')
 
 int mm_init(range_t **ranges);
 void* mm_malloc(size_t size);
@@ -89,7 +93,9 @@ void mm_exit(void);
 
 
 int mm_check(void);
-void *extend_heap(size_t words);
+void printheap(void);
+
+static void *extend_heap(size_t words);
 static void *coalesce(void *bp);
 static void *find_fit(size_t asize);
 static void place(void *bp, size_t asize);
@@ -103,7 +109,7 @@ static void place(void *bp, size_t asize);
 static void remove_range(range_t **ranges, char *lo)
 {
 #ifdef DEBUG
-printf("    remove_range called\n");
+printf("| | remove_range BEGINDONE\n");
 #endif
   range_t *p;
   range_t **prevpp = ranges;
@@ -127,8 +133,8 @@ printf("    remove_range called\n");
 int mm_init(range_t **ranges)
 {
   /* YOUR IMPLEMENTATION */
-#ifdef DEBUG
-printf("  mm_init called\n");
+#ifdef DEBUG_STATUS
+printf("\n| mm_init BEGIN\n");
 #endif
   /* Create the initial empty heap */
   if ((heap_listp = mem_sbrk(4*WSIZE)) == (void *)-1)
@@ -139,6 +145,10 @@ printf("  mm_init called\n");
   PUT(heap_listp + (3*WSIZE), PACK(0, 1));
   heap_listp += (2*WSIZE); // Middle of prologue block
 
+#ifdef DEBUG
+mm_check();
+#endif
+
   /* Extend the empty heap with a free block of CHUNKSIZE bytes */
   if (extend_heap(CHUNKSIZE/WSIZE) == NULL)
     return -1;
@@ -147,7 +157,10 @@ printf("  mm_init called\n");
   gl_ranges = ranges;
 
 #ifdef DEBUG
-printf("  init done\n");
+mm_check();
+#endif
+#ifdef DEBUG_STATUS
+printf("| init DONE\n");
 #endif
 
   return 0;
@@ -159,35 +172,46 @@ printf("  init done\n");
  */
 void* mm_malloc(size_t size)
 {
+#ifdef DEBUG_STATUS
+printf("| mm_malloc BEGIN\n");
+#endif
 #ifdef DEBUG
-printf("  mm_malloc called\n");
+mm_check();
 #endif
   size_t asize;
   size_t extendsize;
   char *bp;
+
+  if (heap_listp == 0) 
+    mm_init(NULL); // WARN: not sure if used correctly
 
   if (size == 0) return NULL;
 
   if (size <= DSIZE) asize = 2*DSIZE;
   else asize = DSIZE * ((size + (DSIZE) + (DSIZE-1)) / DSIZE);
 
+#ifdef DEBUG
+printf("| | malloc - size: %zu, adjusted size: %zu \n", size, asize);
+#endif
+
   if((bp = find_fit(asize)) != NULL) {
+#ifdef DEBUG
+printf("| | found fit block\n");
+#endif
     place(bp, asize);
+#ifdef DEBUG
+printf("| mm_malloc DONE\n");
+#endif
     return bp;
   }
 
   extendsize = MAX(asize, CHUNKSIZE);
   if((bp = extend_heap(extendsize/WSIZE)) == NULL) return NULL;
   place(bp, asize);
+#ifdef DEBUG_STATUS
+printf("| mm_malloc DONE\n");
+#endif
   return bp;
-  /*int newsize = ALIGN(size + SIZE_T_SIZE);*/
-  /*void *p = mem_sbrk(newsize);*/
- /*if (p == (void *)-1)*/
-    /*return NULL;*/
-  /*else {*/
-    /**(size_t *)p = size;*/
-    /*return (void *)((char *)p + SIZE_T_SIZE);*/
-  /*}*/
 }
 
 /*
@@ -196,14 +220,14 @@ printf("  mm_malloc called\n");
 void mm_free(void *ptr)
 {
   /* YOUR IMPLEMENTATION */
-#ifdef DEBUG
-printf("  mm_free called\n");
+#ifdef DEBUG_STATUS
+printf("| mm_free BEGIN\n");
 #endif
-
 #ifdef DEBUG
-  printf("      reaches here?");
+mm_check();
+printf("| | free => ptr: %p\n", ptr);
 #endif
-  size_t size = GET_SIZE(HDRP(ptr));
+  size_t size = BLK_SIZE(ptr);
 
   PUT(HDRP(ptr), PACK(size, 0));
   PUT(FTRP(ptr), PACK(size, 0));
@@ -212,6 +236,12 @@ printf("  mm_free called\n");
   /* DON't MODIFY THIS STAGE AND LEAVE IT AS IT WAS */
   if (gl_ranges)
     remove_range(gl_ranges, ptr);
+#ifdef DEBUG
+mm_check();
+#endif
+#ifdef DEBUG_STATUS
+printf("| mm_free DONE\n");
+#endif
 }
 
 /*
@@ -219,8 +249,8 @@ printf("  mm_free called\n");
  */
 void* mm_realloc(void *ptr, size_t t)
 {
-#ifdef DEBUG
-printf("  !!mm_realloc called\n");
+#ifdef DEBUG_STATUS
+printf("| !!mm_realloc BEGINDONE\n");
 #endif
 
   return NULL;
@@ -231,30 +261,53 @@ printf("  !!mm_realloc called\n");
  */
 void mm_exit(void)
 {
-#ifdef DEBUG
-printf("  mm_exit called\n");
+#ifdef DEBUG_STATUS
+printf("| mm_exit BEGINDONE\n");
 #endif
 
 }
 
-/* EXTRA FUNCTIONS */
+/* ===============================EXTRA FUNCTIONS========================= */
+/* ===============================  FOR TESTING  ========================= */
 /*
  * mm_check - checks heap consistency
  */
 int mm_check(void) {
-#ifdef DEBUG
-printf("   mm_check called\n");
-#endif
+  printheap();
 
   return 0;
 }
 
 /*
+ * printheap - prints out the heap
+ */
+void printheap(void) {
+  printf("\tCURRENT HEAP: ");
+  void *bp = 0;
+  size_t size;
+  char r;
+
+  for(bp = heap_listp; BLK_SIZE(bp) > 0; bp = NEXT_BLKP(bp)) {
+    size = (BLK_SIZE(bp));
+    printf("|%7zu%2c |", size, BLK_ALLOC_CHAR(bp));
+  }
+  printf("\n\t\t      ");
+  for(bp = heap_listp; BLK_SIZE(bp) > 0; bp = NEXT_BLKP(bp)) {
+    printf("|%p|", bp);
+  }
+ 
+  printf("\n");
+  return;
+}
+
+/* ===============================EXTRA FUNCTIONS========================= */
+/*
  * extend_heap - extends the heap
  */
-void *extend_heap(size_t words) {
+static void *extend_heap(size_t words) {
 #ifdef DEBUG
-printf("    extend_heap called\n");
+printf("| | extend_heap BEGIN\n");
+mm_check();
 #endif
 
   char *bp;
@@ -270,8 +323,13 @@ printf("    extend_heap called\n");
   PUT(FTRP(bp), PACK(size, 0)); // Free block footer
   PUT(HDRP(NEXT_BLKP(bp)), PACK(0, 1)); //New epilogue header
 
+  bp = coalesce(bp);
+#ifdef DEBUG
+mm_check();
+printf("| | extend_heap DONE\n");
+#endif
   /* Coalesce if the previous block was free */
-  return coalesce(bp);
+  return bp;
 }
 
 /*
@@ -279,36 +337,53 @@ printf("    extend_heap called\n");
  */
 static void *coalesce(void *bp) {
 #ifdef DEBUG
-printf("    coalesce called\n");
+printf("| | coalesce BEGIN\n");
+printf("| | | prev_blkp:%p:%c, next_blkp: %p:%c\n", PREV_BLKP(bp), BLK_ALLOC_CHAR(PREV_BLKP(bp)), NEXT_BLKP(bp), BLK_ALLOC_CHAR(NEXT_BLKP(bp)));
 #endif
-  size_t prev_alloc = GET_ALLOC(FTRP(PREV_BLKP(bp)));
-  size_t next_alloc = GET_ALLOC(HDRP(NEXT_BLKP(bp)));
-  size_t size = GET_SIZE(HDRP(bp));
+  size_t prev_alloc = BLK_ALLOC(PREV_BLKP(bp));
+  size_t next_alloc = BLK_ALLOC(NEXT_BLKP(bp));
+  size_t size = BLK_SIZE(bp);
 
   if (prev_alloc && next_alloc) { // both not empty
+#ifdef DEBUG
+printf("| | | both not empty\n");
+printf("| | coalesce DONE\n");
+#endif
     return bp;
   }
 
   else if (prev_alloc && !next_alloc) { // next is empty
-    size += GET_SIZE(HDRP(NEXT_BLKP(bp)));
+#ifdef DEBUG
+printf("| | | next is empty\n");
+#endif
+    size += BLK_SIZE(NEXT_BLKP(bp));
     PUT(HDRP(bp), PACK(size, 0));
     PUT(FTRP(NEXT_BLKP(bp)), PACK(size, 0));
   }
 
-  else if (prev_alloc && next_alloc) { // prev is empty
-    size += GET_SIZE(HDRP(PREV_BLKP(bp)));
+  else if (!prev_alloc && next_alloc) { // prev is empty
+#ifdef DEBUG
+printf("| | | prev is empty\n");
+#endif
+    size += BLK_SIZE(PREV_BLKP(bp));
     PUT(HDRP(PREV_BLKP(bp)), PACK(size, 0));
     PUT(FTRP(bp), PACK(size, 0));
     bp = PREV_BLKP(bp);
   }
 
   else { // both are empty
-    size += GET_SIZE(HDRP(PREV_BLKP(bp))) + GET_SIZE(FTRP(NEXT_BLKP(bp)));
+#ifdef DEBUG
+printf("| | | both are empty\n");
+#endif
+    size += BLK_SIZE(PREV_BLKP(bp)) + BLK_SIZE(NEXT_BLKP(bp));
     PUT(HDRP(PREV_BLKP(bp)), PACK(size, 0));
     PUT(FTRP(NEXT_BLKP(bp)), PACK(size, 0));
     bp = PREV_BLKP(bp);
   }
 
+#ifdef DEBUG
+printf("| | coalesce DONE\n");
+#endif
   return bp;
 }
 
@@ -316,8 +391,12 @@ printf("    coalesce called\n");
  * find_fit - 
  */
 static void *find_fit(size_t asize) {
+#ifdef DEBUG
+printf("| | find_fit BEGINDONE\n");
+#endif
+ 
   /* First fit search */
-  void *bp;
+  void *bp = 0;
 
   for(bp = heap_listp; GET_SIZE(HDRP(bp)) > 0; bp = NEXT_BLKP(bp)) {
     if(!GET_ALLOC(HDRP(bp)) && (asize <= GET_SIZE(HDRP(bp)))) {
@@ -331,9 +410,13 @@ static void *find_fit(size_t asize) {
  * place - 
  */
 static void place(void *bp, size_t asize) {
-  size_t csize = GET_SIZE(HDRP(bp));
+#ifdef DEBUG
+printf("| | place BEGIN\n");
+#endif
+ 
+  size_t csize = BLK_SIZE(bp);
 
-  if((csize - asize) <= (2*DSIZE)) {
+  if((csize - asize - (2*DSIZE)) >= 0) {
     PUT(HDRP(bp), PACK(asize, 1));
     PUT(FTRP(bp), PACK(asize, 1));
     bp = NEXT_BLKP(bp);
@@ -343,7 +426,10 @@ static void place(void *bp, size_t asize) {
     PUT(HDRP(bp), PACK(csize, 1));
     PUT(FTRP(bp), PACK(csize, 1));
   }
-
+#ifdef DEBUG
+mm_check();
+printf("| | place DONE\n");
+#endif
 }
 
 
